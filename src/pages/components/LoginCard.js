906c6216784049a6dbedf29ba3649f2fb35b7582
Sudev Suresh Sreedevi <54617167+GameGodS3@users.noreply.github.com>
2022-06-20 15:52:49 +0000

    Added: Login Page Card

--- src/pages/components/LoginCard.js
+++ src/pages/components/LoginCard.js
@@ -1,12 +1,24 @@
 import React from "react";
 
 import styles from "../../styles/LoginCard.module.scss";
+import { Button } from "react-bootstrap";
 
 function LoginCard() {
   return (
     <div className={styles.LoginCard}>
-      <div className={styles.Login_logo}>Mudrika</div>
-      
+      <div className={styles.Login_logo}>
+        Mudrika{" "}
+        <div className={styles.Login_logo_sub}>
+          Disaster Management Infrastructure
+        </div>{" "}
+      </div>
+      <Button size="md" variant="dark" className={styles.Login_Button}>
+        <img src="/metamask.svg" alt="" className={styles.Login_metaicon} />
+        Sign In with MetaMask
+      </Button>
+      <div>
+        No account? <a href="/register">Register Here</a>
+      </div>
     </div>
   );
 }
