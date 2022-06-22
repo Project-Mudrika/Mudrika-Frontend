994cca5f9d49b20c66340a23dfdcaf932f261f29
Sudev Suresh Sreedevi <54617167+GameGodS3@users.noreply.github.com>
2022-06-22 18:34:50 +0000

    Added: newFundRequest page

--- src/pages/manageFunds/newFundRequest.js
+++ src/pages/manageFunds/newFundRequest.js
@@ -1,38 +1,58 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 
+import dashboardStyles from "../../styles/Dashboard.module.scss";
 import bodyStyles from "../../styles/Home.module.scss";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Card } from "react-bootstrap";
 
 function newFundRequest() {
   return (
     <div className={bodyStyles.home}>
       <NavBar />
-      <Form>
-        <Form.Group className="mb-3">
-          <Form.Label>Wallet ID</Form.Label>
-          <Form.Control
-            type="text"
-            placeholder="Enter WalletID"
-            value="02340jf0hh0H234YD"
-            readOnly={true}
-          />
-          {/* <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text> */}
-        </Form.Group>
-
-        <Form.Group className="mb-3">
-          <Form.Label>State WalletID</Form.Label>
-          <Form.Control type="text" placeholder="Enter State" />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Check type="checkbox" label="Check me out" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <Card
+        bg="light"
+        className={dashboardStyles.Dashboard_recent_cases}
+        style={{ padding: "2rem", width: "50vw" }}
+      >
+        <Card.Title>Create a New Fund Request</Card.Title>
+        <Card.Body>
+          <Form>
+            <Form.Group className="mb-3">
+              <Form.Label>Your WalletID</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter WalletID"
+                value="02340jf0hh0H234YD"
+                readOnly={true}
+              />
+              {/* <Form.Text className="text-muted">
+                We'll never share your email with anyone else.
+              </Form.Text> */}
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Higher Authority WalletID</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Enter Higher Authority WalletID"
+              />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Amount Needed</Form.Label>
+              <Form.Control type="number" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label>Request Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                placeholder="Enter Reason for Fund Request"
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
     </div>
   );
 }
