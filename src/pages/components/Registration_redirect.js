145d72f857f24470d525d96a595f32d4cb20e83f
GameGodS3 <sudevssuresh@gmail.com>
2023-02-14 22:47:06 +0530

    Changed: Registration types page

---
+++ src/pages/components/Registration_redirect.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Icon } from "@iconify/react";
+
+import styles from "../../styles/RegistrationCard.module.scss";
+
+function RegistrationCard({ icon, title, description, footer_text, onClick }) {
+  return (
+    <a href="#" className={styles.RegistrationCard} onClick={onClick}>
+      <Card style={{ height: "20rem", width: "20rem" }}>
+        <Card.Body>
+          <div className="d-flex justify-content-center align-items-center p-4">
+            <Icon icon={icon} width="4rem" />
+          </div>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text className="text-muted">{description}</Card.Text>
+        </Card.Body>
+        <Card.Footer>
+          <small className="text-muted">{footer_text}</small>
+        </Card.Footer>
+      </Card>
+    </a>
+  );
+}
+
+export default RegistrationCard;
--- src/pages/register/index.js
+++ src/pages/register/index.js
@@ -1,14 +1,46 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-import RegisterForm from "../components/RegisterForm";
+import OfficerRegisterForm from "../components/OfficerRegisterForm";
+import RegistrationCard from "../components/Registration_redirect";
 
 import styles from "../../styles/Home.module.scss";
+import Router from "next/router";
 
 function Register() {
+  const handleOfficerClick = () => {
+    Router.push("/register/officer-registration");
+  };
+  const handleVolunteerClick = () => {};
+
   return (
     <div className={styles.home}>
       <NavBar loginPage={true} />
-      <RegisterForm />
+      {/* <RegisterForm /> */}
+      <div
+        style={{
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "space-evenly",
+        }}
+      >
+        <RegistrationCard
+          type="officer"
+          icon="material-symbols:admin-panel-settings"
+          title="Officer Registration"
+          description="Register as an officer to gain access to Disaster Management Authority tools and resources."
+          footer_text="Requires token from Adminstrator"
+          onClick={handleOfficerClick}
+        />
+        <RegistrationCard
+          type="volunteer"
+          icon="material-symbols:volunteer-activism-rounded"
+          title="Volunteer / Organization Registration"
+          description="Register as a volunteer or organization to contribute to disaster relief efforts."
+          footer_text="Requires Metamask Wallet Account"
+          onClick={handleVolunteerClick}
+        />
+      </div>
     </div>
   );
 }
