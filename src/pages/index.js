7bf4e051757e89f98a6869dbe7701250cde5fe66
GameGodS3 <sudevssuresh@gmail.com>
2023-02-21 10:36:37 +0530

    Changed: login rerouting

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,19 +22,17 @@ export default function Home() {
   useEffect(() => {
     if (address) {
       console.log("Address present", address);
-      // userDetails.fetchUserData(address).then((response) => {
-      //   const userData = response.data.data;
-      //   if (userData.length > 0) {
-      //     console.log("Address exists and valid");
-      //     Router.push("/dashboard");
-      //   } else {
-      //     console.log("Address exists but invalid");
-      //     alert("Unregistered user. Please register first");
-      //   }
-      // }
-      // );
-      Router.push("/dashboard");
-
+      userDetails.fetchUserData(address).then((response) => {
+        const userData = response.data.data;
+        if (userData.length > 0) {
+          console.log("Address exists and valid");
+          Router.push("/dashboard");
+        } else {
+          console.log("Address exists but invalid");
+          alert("Unregistered user. Please register first");
+        }
+      });
+      // Router.push("/dashboard");
     }
   }, [address]);
 
