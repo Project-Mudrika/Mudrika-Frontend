f04e0d35f503415eb5c88f21f55e46e7ca0f9a7b
Deepudeepthi555 <deepikamohandeepz99@gmail.com>
2023-03-18 14:27:53 +0530

    Search by caseId

--- src/pages/components/SearchByCaseIdForm.js
+++ src/pages/components/SearchByCaseIdForm.js
@@ -1,5 +1,5 @@
 import { useWeb3 } from "@3rdweb/hooks";
-import { Modal, Button, Card, Form, Spinner } from "react-bootstrap";
+import { Modal, Button, Card, Form, Spinner, Table } from "react-bootstrap";
 import { Icon } from "@iconify/react";
 import React, { useState } from "react";
 import axios from "axios";
@@ -8,17 +8,56 @@ import dashStyles from "../../styles/Dashboard.module.scss";
 import { pinFileToIPFS } from "../../helpers/uploadIpfs";
 import Router from "next/router";
 
-function SearchByCaseIdForm(){
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const submitHandler = async (e) => {
-        // e.preventDefault();
-        // setIsSubmitting(true);
-        setcaseid("");
-    };
-    return (
+function SearchByCaseIdForm() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [caseId, setCaseId] = useState("");
+  const tableData = {
+    "case_id": "123456",
+    "transactions": [
+      {
+        "type": "consignment",
+        "from": {
+          "type": "national",
+          "name": "National Disaster Management Authority"
+        },
+        "to": {
+          "type": "state",
+          "name": "State Disaster Management Authority",
+          "state": "Kerala"
+        },
+        "amount": 500.00,
+        "timestamp": 1647590400,
+        "consignment_name": "Food Supplies"
+      },
+      {
+        "type": "funds",
+        "from": {
+          "type": "state",
+          "name": "State Disaster Management Authority",
+          "state": "Kerala"
+        },
+        "to": {
+          "type": "district",
+          "name": "District Disaster Management Authority",
+          "state": "Kerala",
+          "district": "Ernakulam"
+        },
+        "amount": 1000.00,
+        "timestamp": 1646414400,
+        "reason": "Disaster response"
+      }
+    ]
+  }
 
-        // <div className={dashStyles.Dashboard}>
-        <div>
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    setIsSubmitting(true);
+    setCaseId("");
+  };
+  return (
+
+    // <div className={dashStyles.Dashboard}>
+    <div>
       <Card
         bg="light"
         className={dashStyles.Dashboard_recent_cases}
@@ -28,29 +67,81 @@ function SearchByCaseIdForm(){
         <Card.Body>
           <Form onSubmit={submitHandler}>
             <Form.Group className="mb-3">
-            <Form.Label>Enter CaseId</Form.Label>
+              <Form.Label>Enter CaseId</Form.Label>
               <Form.Control
                 type="text"
                 placeholder="Enter CaseId"
                 name="caseid"
-                onInput={(e) => setcaseid(e.target.value)}
+                onInput={(e) => setCaseId(e.target.value)}
                 required
               />
             </Form.Group>
             <Form.Group>
-            <Button variant="primary" disabled={isSubmitting} type="submit">
-              {isSubmitting ? (
-                <Spinner animation="border" role={"status"} size="sm" />
-              ) : (
-                "Search"
-              )}
-            </Button>
+              <Button variant="primary" disabled={isSubmitting} type="submit">
+                {isSubmitting ? (
+                  <Spinner animation="border" role={"status"} size="sm" />
+                ) : (
+                  "Search"
+                )}
+              </Button>
             </Form.Group>
-            </Form>
-            </Card.Body>
-            </Card>
-      
-     </div>
+          </Form>
+        </Card.Body>
+
+
+        <Table striped bordered hover>
+          <thead className="text-center">
+
+            <tr>
+              <th>Sl.No</th>
+              <th>Sender</th>
+              <th>Recipient</th>
+              <th>Transaction Type</th>
+              <th>Amount</th>
+              <th>Timestamp</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              tableData?.transactions?.map((transaction, index) => {
+                const timestamp = new Date(transaction.timestamp)
+                return (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>
+                      <div style={{ fontWeight: "bold" }}>{transaction.from.name}</div>
+                      <div className="mt-1" style={{ display: "flex", justifyContent: "space-between", fontSize: "14px" }}>
+                        {transaction.from.district ?
+                          <div>{transaction.from.district}</div> : null
+                        }
+                        {transaction.from.state ?
+                          <div className="ms-auto">{transaction.from.state}</div> : null
+                        }
+                      </div>
+                    </td>
+                    <td>
+                      <div style={{ fontWeight: "bold" }}>{transaction.to.name}</div>
+                      <div className="mt-1" style={{ display: "flex", justifyContent: "space-between", fontSize: "14px" }}>
+                        {transaction.to.district ?
+                          <div>{transaction.to.district}</div> : null
+                        }
+                        {transaction.to.state ?
+                          <div className="ms-auto">{transaction.to.state}</div> : null
+                        }
+                      </div>
+                    </td>
+                    <td>{transaction.type}</td>
+                    <td>{transaction.amount}</td>
+                    <td>{timestamp.toUTCString()}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </Table>
+      </Card>
+
+    </div>
   );
 }
 export default SearchByCaseIdForm;
