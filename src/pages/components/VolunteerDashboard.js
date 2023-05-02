409acd2b413a36a13e5415c8795d703911a3249f
GameGodS3 <sudevssuresh@gmail.com>
2023-05-02 20:24:28 +0530

    Added: Add new activity modal and success toast

--- src/pages/components/VolunteerDashboard.js
+++ src/pages/components/VolunteerDashboard.js
@@ -1,20 +1,73 @@
 import React, { useState, useEffect } from "react";
-import { Button, Card } from "react-bootstrap";
+import {
+  Button,
+  Card,
+  Modal,
+  Form,
+  Toast,
+  Spinner,
+  ToastContainer,
+} from "react-bootstrap";
+import { Icon } from "@iconify/react";
+import { useStoreState } from "easy-peasy";
 
 import Image from "next/image";
 import styles from "../../styles/Dashboard.module.scss";
 import volunteerStyles from "../../styles/Volunteer.module.scss";
-import { useStoreState } from "easy-peasy";
+import { pinFileToIPFS } from "../../helpers/uploadIpfs";
 
 function VolunteerDashboard() {
   const userDetails = useStoreState((state) => state.userData);
   const userData = userDetails.data[0];
 
-  const [profileImg, setProfileImg] = useState("");
+  // Receive image from form
+  const [activityUploadImage, setActivityUploadImage] = useState(null);
+  const handleImageChange = (e) => {
+    setActivityUploadImage(e.target.files[0]);
+  };
 
-  useEffect(() => {
-    setProfileImg(userData.profileimg);
-  }, [userData]);
+  // Field lock on submitting
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  // Success state
+  const [isSuccess, setIsSuccess] = useState(false);
+
+  // Server submission data
+  const [description, setDescription] = useState("");
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    setIsSubmitting(true);
+
+    // Get image link by uploading to IPFS
+    const fileUrl = await pinFileToIPFS(activityUploadImage);
+    console.log(fileUrl);
+
+    // Construct the activity FormData object
+    const formData = new FormData();
+    formData.append("description", description);
+    formData.append("tokenCount", 0);
+    formData.append("date", Date.now());
+    formData.append("imageLink", fileUrl);
+
+    // Do something with the activity data and image file, such as send it to a server
+    // Use formData instead of activity JSON object
+    for (let pair of formData.entries()) {
+      console.log(pair[0] + ": " + pair[1]);
+    }
+
+    // Reset the form
+    event.target.reset();
+    setDescription("");
+    setActivityUploadImage(null);
+    setIsSubmitting(false);
+    setIsSuccess(true);
+  };
+
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClose = () => setShowModal(false);
+  const handleShow = () => setShowModal(true);
 
   return (
     <div className={styles.Dashboard}>
@@ -111,10 +164,82 @@ function VolunteerDashboard() {
             <Button
               variant="primary"
               style={{ float: "right" }}
-              onClick={() => {}}
+              onClick={handleShow}
             >
               Add Activity
             </Button>
+            {/* Add Activity Modal */}
+            <Modal show={showModal} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>Add Activity</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form onSubmit={handleFormSubmit} id="activityForm">
+                  <Form.Group className="mb-3">
+                    <Form.Label>Activity Description</Form.Label>
+                    <Form.Control
+                      disabled={isSubmitting}
+                      as="textarea"
+                      rows={3}
+                      placeholder="Describe what the activity is about"
+                      onChange={(e) => setDescription(e.target.value)}
+                      value={description}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3">
+                    <Form.Label>Activity Images</Form.Label>
+                    <Form.Control
+                      disabled={isSubmitting}
+                      type="file"
+                      accept="image/*"
+                      onChange={handleImageChange}
+                      required
+                    />
+                  </Form.Group>
+                </Form>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button
+                  variant="secondary"
+                  onClick={handleClose}
+                  disabled={isSubmitting}
+                >
+                  Cancel
+                </Button>
+                <Button
+                  variant="primary"
+                  form="activityForm"
+                  disabled={isSubmitting}
+                  type="submit"
+                >
+                  {isSubmitting ? (
+                    <Spinner animation="border" />
+                  ) : (
+                    "Post Activity"
+                  )}
+                </Button>
+              </Modal.Footer>
+              <ToastContainer className="p-3" position="bottom-end">
+                <Toast
+                  bg="success"
+                  className="text-white"
+                  onClose={() => setIsSuccess(false)}
+                  show={isSuccess}
+                  delay={3000}
+                  autohide
+                >
+                  <Toast.Header>
+                    <Icon
+                      icon="mdi:tick-circle-outline"
+                      color="#408558"
+                      className="mx-1"
+                    />
+                    <strong className="me-auto">Success!</strong>
+                  </Toast.Header>
+                  <Toast.Body>Successfully posted new activity</Toast.Body>
+                </Toast>
+              </ToastContainer>
+            </Modal>
           </Card.Body>
         </Card>
       </div>
